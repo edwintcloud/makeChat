e59b863452e0daeee3f790ba11e7562d0aed2cf5
Edwin Cloud <ecloud412@gmail.com>
2019-01-30 16:05:01 -0800

    Users can join chat and send messages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,56 @@
 window.onload = () => {
-
   // connect to socket.io server
   const socket = io.connect();
 
+  // keep track of current user
+  let currentUser;
+
   // create user onclick
-  document.getElementById("createUserBtn").addEventListener('click', (e) => {
+  document.getElementById("createUserBtn").addEventListener("click", e => {
     e.preventDefault();
     const element = document.getElementById("usernameInput");
-    const username = element.value;
-    if(username.length > 0){
+    currentUser = element.value;
+    if (currentUser.length > 0) {
       //Emit to the server the new user
-      socket.emit('new user', username);
-      element.parentNode.removeChild(element);
+      socket.emit("new user", currentUser);
+      const form = document.querySelector(".usernameForm");
+      form.parentNode.removeChild(form);
+      // make main page visible
+      document.querySelector(".mainContainer").style.display = "flex";
+    }
+  });
+
+  // sendchat onclick
+  document.getElementById("sendChatBtn").addEventListener("click", e => {
+    e.preventDefault();
+    const element = document.getElementById("chatInput");
+    const message = element.value;
+    if (message.length > 0) {
+      //Emit to the server the new user
+      socket.emit("new message", {
+        sender: currentUser,
+        message: message
+      });
+      element.value = "";
     }
   });
 
   //socket listeners
-  socket.on('new user', (username) => {
+  socket.on("new user", username => {
     console.log(`✋ ${username} has joined the chat! ✋`);
+    // add the new user to the .usersOnline div
+    document.querySelector(
+      ".usersOnline"
+    ).innerHTML += `<div class="userOnline">${username}</div>`;
   });
-  
+
+  //Output the new message
+  socket.on("new message", data => {
+    document.querySelector(".messageContainer").innerHTML += `
+    <div class="message">
+      <p class="messageUser">${data.sender}: </p>
+      <p class="messageText">${data.message}</p>
+    </div>
+  `
+  });
 };
--- server.js
+++ server.js
@@ -19,6 +19,13 @@ io.on("connection", (socket) => {
     //Send the username to all clients currently connected
     io.emit("new user", username);
   });
+
+  //Listen for new messages
+  socket.on('new message', (data) => {
+    // Send that data back to ALL clients
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+    io.emit('new message', data);
+  })
 })
 
 // set view engine to ejs
